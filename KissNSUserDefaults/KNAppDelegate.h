5d9d1afa1c55aefcdb9d2ab5e8c35d9d220ba5fd
Chen Xian'an <xianan.chen@gmail.com>
2013-01-01 11:56:41 +0800

    Initial Commit

---
+++ KissNSUserDefaults/KNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KNAppDelegate.h
+//  KissNSUserDefaults
+//
+//  Created by Chen Xian'an on 1/1/13.
+//  Copyright (c) 2013 lazyapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KissNSUserDefaults/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KissNSUserDefaults
+//
+//  Created by Chen Xian'an on 1/1/13.
+//  Copyright (c) 2013 lazyapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([KNAppDelegate class]));
+  }
+}
